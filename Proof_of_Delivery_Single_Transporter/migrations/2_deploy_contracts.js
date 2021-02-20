670b85fdd5f75466735acc920bf2cbb65bbf48f8
vipul malhotra <vipulmalhotra1996@gmail.com>
2021-02-19 19:21:26 -0500

    Delete Proof_of_Delivery_Single_Transporter directory

--- Proof_of_Delivery_Single_Transporter/migrations/2_deploy_contracts.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-var POD = artifacts.require('./POD.sol');
-module.exports = function(deployer, network, accounts){
- deployer.deploy(POD, accounts[1],accounts[2],accounts[3],accounts[4],accounts[5],'1');
-};
